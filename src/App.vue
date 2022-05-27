fcf1d70fd840136376fc8efefc763c6816ecefa7
Draacckeer <u201922975@upc.edu.pe>
2022-05-27 03:54:00 -0500

    fixed: app navbar

--- src/App.vue
+++ src/App.vue
@@ -9,39 +9,7 @@ export default {
 </script>
 
 <template>
-  <pv-toast></pv-toast>
-  <header>
-    <pv-toolbar class="toolbar bg-primary">
-      <template #start>
-        <img
-          alt="Getting Better logo"
-          class="logo"
-          src="@/assets/logo-gb.png"
-          width="150"
-          height="85"
-        />
-      </template>
-      <template #end>
-        <div class="flex-column">
-          <router-link
-            v-for="item in items"
-            :to="item.to"
-            custom
-            v-slot="{ navigate, href }"
-            :key="item.label"
-
-          >
-            <pv-button
-              class="p-button"
-              :href="href"
-              @click="navigate"
-              >{{ item.label }}</pv-button
-            >
-          </router-link>
-        </div>
-      </template>
-    </pv-toolbar>
-  </header>
+  
   <RouterView />
 </template>
 
